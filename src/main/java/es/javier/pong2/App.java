94cae4969a7f362189f2cdf52c2cf8081cee4f7f
Javier <Javier@192.168.10.47>
2020-01-10 10:25:06 +0100

    bola en pantalla

--- src/main/java/es/javier/pong2/App.java
+++ src/main/java/es/javier/pong2/App.java
@@ -2,8 +2,8 @@ package es.javier.pong2;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -12,14 +12,20 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    public void start(Stage stage) { 
+        StackPane root = new StackPane();
+        var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
+        
+        Circle circleBall = new Circle();
+        circleBall.setCenterX(10);
+        circleBall.setCenterY(30);
+        circleBall.setRadius(7);
+        //Circle circleBall2 = new Circle(10,30,7);
+        
+        root.getChildren().add(circleBall);
+        
     }
 
     public static void main(String[] args) {
